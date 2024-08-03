3b08344d520a880631cb0f72173871f34dc28827
Sarah <sarah.legrand06@laposte.net>
2024-08-04 01:43:23 +0200

    erreur id gérée avec navigate

--- src/pages/rental.js
+++ src/pages/rental.js
@@ -2,18 +2,20 @@ import React from 'react';
 import Rentalslideshow from '../components/rental-slideshow';
 import Rentaldetails from '../components/rental-details';
 import Collapse from '../components/collapse';
-import { useParams } from 'react-router-dom';
+import { useParams, Navigate } from 'react-router-dom';
 import data from '../assets/data/logements.json';
 import '../styles/rental.scss';
 
 
 const Rental = () => {
-
   const { id } = useParams();
   const rental = data.find(item => item.id === id)
-  const { description, equipments } = rental;
+  
+  if (!rental) {
+    return <Navigate to="*"/>;
+  }
 
-  console.log(description);
+  const { description, equipments } = rental;
 
   return (
     <div className="rental">
